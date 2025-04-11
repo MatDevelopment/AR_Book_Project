accc962cf6906f89391ded9d0e6ce1d644448bf1
MatDevelopment <95708125+MatDevelopment@users.noreply.github.com>
2025-04-11 13:40:30 +0200

    "Animated" solar system
    Implemented a solar system that closely resembles the real life orbital trajectory speeds and spinning speeds.
    Prefab is called SolarSystem_SunCenterRotation.
    It is done through rotations in scripts and therefore speeds can be adjusted in the inspector window under each planet.

---
+++ Assets/RotateAroundCenter.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+public class RotateAroundCenter : MonoBehaviour
+{
+    [SerializeField] private GameObject SunGameobject_SolarSystemCenter;
+
+    [SerializeField] private float _degreesPerSecondToRotateAroundSun;
+    [SerializeField] private float _degreesPerSecondToRotateSelf;
+    //[SerializeField] private Vector3 selfRotateVector;
+    
+
+    private void FixedUpdate()
+    {
+        gameObject.transform.RotateAround(SunGameobject_SolarSystemCenter.transform.position,Vector3.up, _degreesPerSecondToRotateAroundSun * Time.deltaTime);
+        gameObject.transform.Rotate(Vector3.up, _degreesPerSecondToRotateSelf * Time.deltaTime, Space.Self);
+        
+        
+    }
+}
+
+
